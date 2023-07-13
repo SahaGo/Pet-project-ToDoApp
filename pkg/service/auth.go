93e66fb24e5d39e49e7646836528c90af1890c5e
Aleksandra P <sahapanova@yandex.ru>
2023-07-13 23:13:41 +0300

    upd

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,9 +5,20 @@ import (
 	"Pet-project-ToDoApp/pkg/repository"
 	"crypto/sha1"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
-const salt = "csiugfioieu7syf8r9w8er9we8fy"
+const (
+	salt       = "csiugfioieu7syf8r9w8er9we8fy"
+	signingKey = "jfjerT#UT%#jfjwefR#$UT3J$@%"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -21,6 +32,23 @@ func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err //если юзера нет, то вергем ошибку
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{ // если есть, сгенерируем токен
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(), //через сколько сгорит токен
+			IssuedAt:  time.Now().Unix(),               //время генерации токена
+		},
+		UserId: user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
