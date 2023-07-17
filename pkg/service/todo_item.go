203de8abba5843e292afa74bb123b60af94a533d
Aleksandra P <sahapanova@yandex.ru>
2023-07-17 21:31:45 +0300

    upd

---
+++ pkg/service/todo_item.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	todo "Pet-project-ToDoApp"
+	"Pet-project-ToDoApp/pkg/repository"
+)
+
+type TodoItemService struct {
+	repo     repository.TodoItem
+	listRepo repository.TodoList
+}
+
+func NewTodoItemService(repo repository.TodoItem, listRepo repository.TodoList) *TodoItemService {
+	return &TodoItemService{repo: repo, listRepo: listRepo}
+}
+
+func (s *TodoItemService) Create(userId, listId int, item todo.TodoItem) (int, error) {
+	_, err := s.listRepo.GetById(userId, listId)
+	if err != nil {
+		//list does not exists or does not belongs to user
+		return 0, err
+	}
+
+	return s.repo.Create(listId, item)
+}
+
+func (s *TodoItemService) GetAll(userId, listId int) ([]todo.TodoItem, error) {
+	return s.repo.GelAll(userId, listId)
+}
+
+func (s *TodoItemService) GetById(userId, itemId int) (todo.TodoItem, error) {
+	return s.repo.GetById(userId, itemId)
+}
